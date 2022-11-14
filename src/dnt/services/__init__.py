29be4ab90d30616a28fb29c820d176ab4576ef2e
xg1990 <xg1990@163.com>
2022-11-14 17:06:35 +1100

    smtp service

--- src/dnt/services/__init__.py
+++ src/dnt/services/__init__.py
@@ -1,4 +1,5 @@
 from dnt.services.dummy import ConsoleDummyService
+from dnt.services.email import SMTPService
 from dnt.services.sql import SQLSource
 
-__all__ = ["ConsoleDummyService", "SQLSource"]
+__all__ = ["ConsoleDummyService", "SQLSource", "SMTPService"]
